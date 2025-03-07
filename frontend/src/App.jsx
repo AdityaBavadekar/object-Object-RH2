e3d22c4ca7239e224cbbe8a115a2efcf45affb95
dhruvl2006 <lohar.dhruv07@gmail.com>
2025-03-07 16:18:00 +0530

    in app.jsx if user is not present then it will redirect the user to login not lol as it is not a defined route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 			<BrowserRouter>
 				<Navbar />
 				<Routes>
-					<Route path="/" element={user ? <Home /> : <Navigate to="/" />} />
+					<Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
 					<Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
 					<Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
 				</Routes>
